b3a2608413ce0cbdf31a113802bdab108c85f5d7
fidashn <87206839+fidashn@users.noreply.github.com>
2023-08-14 16:38:32 +0300

    created basic app + video background

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import videoBg from './drone.mp4';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="description">
+        <h2>WingWarden</h2>
+       <p>Are you tired of birds causing disturbances around your property? <br />
+       Say hello to serenity with WingWarden, your ultimate solution for bird control. <br /> Using state-of-the-art drone technology, WingWarden ensures that your home remains free from unwanted avian visitors.</p>
+        <p> 
+         <h3>Discover the WingWarden Advantage:</h3>
+          Why WingWarden? <br />
+          1.🔒 Effective Deterrence: Unlike traditional methods, WingWarden takes a proactive approach. Our drones create an environment that birds naturally avoid, preventing nests, droppings, and disturbances. <br />
+          2.🌿 Preserving Nature: WingWarden acknowledges the importance of maintaining the ecological balance. Our solution allows birds to flourish in their natural habitats while keeping them away from human living spaces.<br />
+         <p>🌐 Connect With Us: Stay updated with WingWarden news, tips, and success stories by following us on social media. <br /></p> 
+         <p> Get in Touch:
+          Ready to take back control of your property?<br />
+          Contact us today to learn more about how WingWarden can revolutionize your living space.</p>
+        </p>
+      </div>
+      <video src={videoBg} muted loop autoPlay className='bg-video'></video>
+    </div>
+  );
+}
+
+export default App;
