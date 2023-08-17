5d3d74d22d3f9e60527ef276e81ec9a485346538
fidashn <87206839+fidashn@users.noreply.github.com>
2023-08-17 23:18:02 +0300

    styling changes

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="description">
-        <h2>WingWarden</h2>
+      <h2>WingWarden</h2>
        <p>Are you tired of birds causing disturbances around your property? <br />
        Say hello to serenity with WingWarden, your ultimate solution for bird control. <br /> Using state-of-the-art drone technology, WingWarden ensures that your home remains free from unwanted avian visitors.</p>
         <p> 
-         <h3>Discover the WingWarden Advantage:</h3>
+        <h3>Discover the WingWarden Advantage:</h3>
           Why WingWarden? <br />
           1.🔒 Effective Deterrence: Unlike traditional methods, WingWarden takes a proactive approach. Our drones create an environment that birds naturally avoid, preventing nests, droppings, and disturbances. <br />
           2.🌿 Preserving Nature: WingWarden acknowledges the importance of maintaining the ecological balance. Our solution allows birds to flourish in their natural habitats while keeping them away from human living spaces.<br />
          <p>🌐 Connect With Us: Stay updated with WingWarden news, tips, and success stories by following us on social media. <br /></p> 
          <p> Get in Touch:
           Ready to take back control of your property?<br />
-          Contact us today to learn more about how WingWarden can revolutionize your living space.</p>
+          Contact us today to learn more about how WingWarden can revolutionize your living space. <br />
+          <a href="https://b2ob9pl3hi0.typeform.com/to/Vg9eofIC"  target="_blank"> tell us what you think</a></p>
         </p>
       </div>
+      <div className="video-wrapper">
       <video src={videoBg} muted loop autoPlay className='bg-video'></video>
+      </div>
     </div>
   );
 }
